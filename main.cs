d93561cc4d714cf15d49a030f5f7f742375c107d
ChristianOlmedo <77959559+ChristianOlmedo@users.noreply.github.com>
2021-08-20 15:28:57 +0200

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Math.Round(Math.PI, 2));
+        Console.WriteLine(Math.Round(Math.PI, 3));
+        Console.WriteLine(Math.Round(Math.PI, 4));
+        Console.WriteLine(Math.Round(Math.PI, 5));
+        Console.WriteLine(Math.PI);
+    }
+}
